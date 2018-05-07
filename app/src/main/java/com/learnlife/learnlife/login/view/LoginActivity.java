32c9b654a469c32a62e228c23f2b450bbc197886
Adama <a.traore.pro@gmail.com>
2018-05-07 18:11:41 +0200

    replacing Firebase by AndroidNetworking

--- app/src/main/java/com/learnlife/learnlife/login/view/LoginActivity.java
+++ app/src/main/java/com/learnlife/learnlife/login/view/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -15,12 +16,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.learnlife.learnlife.R;
 import com.learnlife.learnlife.home.view.HomeActivity;
+
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -37,6 +43,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private Animation anim; //Le faire dans une classe mère pour pas le répéter à chaque button
     private boolean isIncomplete; //boolean pour savoir si les champs sont tous remplis ou pas
+    private final String Tag = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +64,8 @@ public class LoginActivity extends AppCompatActivity {
         view.startAnimation(anim);
         prbLogin.setVisibility(View.VISIBLE);
 
-        String email = edtEmail.getText().toString();
-        String password = edtPassword.getText().toString();
+        final String email = edtEmail.getText().toString();
+        final String password = edtPassword.getText().toString();
 
         //Check si les edittexts sont vides ou 0-length
         if(TextUtils.isEmpty(email)) {
@@ -75,6 +82,26 @@ public class LoginActivity extends AppCompatActivity {
             prbLogin.setVisibility(View.GONE);
             return;
         }
+
+        AndroidNetworking.post("http://localhost:8080/login")
+                .addBodyParameter("email", email)
+                .addBodyParameter("password", password)
+                .setTag("login")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(LoginActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                        Log.d(Tag, "Login OK with user email: "+email+" & password: "+password);
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Toast.makeText(LoginActivity.this, "NOT OK", Toast.LENGTH_SHORT).show();
+                        Log.d(Tag, "Login NOT OK with user email: "+email+" & password: "+password);
+                    }
+                });
 
     }
 
--- app/src/main/java/com/learnlife/learnlife/login/view/RegisterActivity.java
+++ app/src/main/java/com/learnlife/learnlife/login/view/RegisterActivity.java
@@ -2,14 +2,76 @@ package com.learnlife.learnlife.login.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.learnlife.learnlife.R;
 
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    /***********************************************************
+     *  Attributes
+     **********************************************************/
+    @BindView(R.id.edtFirstname) EditText edtFirstname;
+    @BindView(R.id.edtPassword) EditText edtPassword;
+    @BindView(R.id.edtEmail) EditText edtEmail;
+    @BindView(R.id.edtConfirmPassword) EditText edtConfirmPassword;
+    @BindView(R.id.edtLastname) EditText edtLastname;
+
+    private final String Tag = getClass().getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ButterKnife.bind(this);
+
+
+    }
+
+
+
+    public void imbRegisterEvent(View view){
+        final String email = edtEmail.getText().toString();
+        final String password = edtPassword.getText().toString();
+        final String firstname = edtFirstname.getText().toString();
+        final String lastname = edtLastname.getText().toString();
+
+
+        AndroidNetworking.post("http://localhost:8080/users")
+                .addBodyParameter("email", email)
+                .addBodyParameter("password", password)
+                .addBodyParameter("firstname", firstname)
+                .addBodyParameter("lastname", lastname)
+                .setTag("register")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(RegisterActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                        Log.d(Tag, "Register OK with user email: "+email+" & password: "+password);
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Toast.makeText(RegisterActivity.this, "NOT OK", Toast.LENGTH_SHORT).show();
+                        String error = anError.getMessage();
+                        Log.d(Tag, anError.getErrorBody()+ " ");
+                    }
+                });
     }
 }
