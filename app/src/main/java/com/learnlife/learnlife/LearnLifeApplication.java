bc9700479da405de83a1265996760c31f0e33716
Adama <a.traore.pro@gmail.com>
2018-06-06 16:36:58 +0200

    chips tags and ajout dans une liste

--- app/src/main/java/com/learnlife/learnlife/LearnLifeApplication.java
+++ app/src/main/java/com/learnlife/learnlife/LearnLifeApplication.java
@@ -6,20 +6,12 @@ import com.androidnetworking.AndroidNetworking;
 
 public class LearnLifeApplication extends Application {
 
-    public static final String BASE_URL = "http://192.168.1.20:8080";
-    public static String idUser;
-    public static String token;
-
-    private static LearnLifeApplication instance;
+    public static final String BASE_URL = "http://192.168.100.83:8080";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        instance = this;
-        AndroidNetworking.initialize(getApplicationContext());
-    }
 
-    public static LearnLifeApplication getInstance() {
-        return instance;
+        AndroidNetworking.initialize(getApplicationContext());
     }
 }
