7c4173b269c6bb1933f33eb0e2f4a55802d7a725
Cédric Nanteau <cedric.nanteau@gmail.com>
2018-06-09 17:40:07 +0200

    the first activity to launch is now Main, and if the user is not loddeg in, send him back to the login page

--- app/src/main/java/com/learnlife/learnlife/Main/view/MainActivity.java
+++ app/src/main/java/com/learnlife/learnlife/Main/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.learnlife.learnlife.Main.view;
+package com.learnlife.learnlife.main.view;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
