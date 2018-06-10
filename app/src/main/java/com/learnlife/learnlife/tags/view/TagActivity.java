bfce39cedd08375530ba95166f63c566a2dd4ec4
Adama <a.traore.pro@gmail.com>
2018-06-10 18:19:08 +0200

    error correction

--- app/src/main/java/com/learnlife/learnlife/tags/view/TagActivity.java
+++ app/src/main/java/com/learnlife/learnlife/tags/view/TagActivity.java
@@ -18,7 +18,7 @@ import com.androidnetworking.interfaces.ParsedRequestListener;
 import com.beloo.widget.chipslayoutmanager.ChipsLayoutManager;
 import com.beloo.widget.chipslayoutmanager.SpacingItemDecoration;
 import com.learnlife.learnlife.LearnLifeApplication;
-import com.learnlife.learnlife.main.view.MainActivity;
+import com.learnlife.learnlife.Main.view.MainActivity;
 import com.learnlife.learnlife.R;
 import com.learnlife.learnlife.crosslayers.utils.Dialog;
 import com.learnlife.learnlife.login.view.LoginActivity;
