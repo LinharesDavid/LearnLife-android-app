8b1f14a15368217b474912e90dc36c167dc29bea
Cédric Nanteau <cedric.nanteau@gmail.com>
2018-06-10 10:54:16 +0200

    transformed Login to MVP

---
+++ app/src/main/java/com/learnlife/learnlife/login/view/ILoginView.java
@@ -0,0 +1,8 @@
+package com.learnlife.learnlife.login.view;
+
+public interface ILoginView {
+
+    void loginSucceed();
+
+    void loginFailed(String error);
+}
---
+++ app/src/main/java/com/learnlife/learnlife/login/view/LoginPresenter.java
@@ -0,0 +1,76 @@
+package com.learnlife.learnlife.login.view;
+
+import android.util.Log;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.learnlife.learnlife.LearnLifeApplication;
+import com.learnlife.learnlife.SessionManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginPresenter implements ILoginPresenter {
+
+    private ILoginView mainView;
+
+    private final String TAG = getClass().getSimpleName();
+
+    private static final String RESPONSE_KEY_USER_ID = "_id";
+    private static final String RESPONSE_KEY_USER_EMAIL = "email";
+    private static final String RESPONSE_KEY_USER_FIRSTNAME = "firstname";
+    private static final String RESPONSE_KEY_USER_LASTNAME = "lastname";
+
+    public LoginPresenter(ILoginView mainView) {
+        this.mainView = mainView;
+    }
+
+    @Override
+    public void loginUser(String email, String password) {
+        JSONObject user = new JSONObject();
+        try {
+            user.put("email", email);
+            user.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        AndroidNetworking.post(LearnLifeApplication.BASE_URL + "/auth/login")
+                .addJSONObjectBody(user)
+                .setTag("login")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, "Login succeeded");
+                        try {
+
+                            JSONObject user = response.getJSONObject("user");
+                            SessionManager.getInstance().createLoginSession(
+                                    user.getString(RESPONSE_KEY_USER_ID),
+                                    user.getString(RESPONSE_KEY_USER_EMAIL),
+                                    user.getString(RESPONSE_KEY_USER_FIRSTNAME),
+                                    user.getString(RESPONSE_KEY_USER_LASTNAME)
+                            );
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        mainView.loginSucceed();
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        String errorBody = anError.getErrorBody() != null ? anError.getErrorBody() : "error without content";
+                        Log.d(TAG, "Login failed : " + errorBody);
+                        mainView.loginFailed(errorBody);
+                    }
+                });
+
+    }
+}
